292d40515b6ad611712cfcc032fd806a16bdeb15
Boomerang24 <alexcomoed@gmail.com>
2021-12-01 00:36:06 -0600

    Designing Layout/Screens

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-es';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es-mx'; // Se importa el idioma de local
+
+moment.locale('es'); // Se escoje el idioma de moment
+
+const localizer = momentLocalizer(moment);
+
+const myEventsList = [{  
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add( 2, 'hours' ).toDate(),
+    bgcolor: '#fafafa',
+    notes: 'Comprar pastel'
+}]
+
+export const CalendarScreen = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    };
+
+    return (
+        <div className="calendar-screen">
+            <Navbar />
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
+            />
+        </div>
+    )
+}
---
+++ src/routes/AppRouter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+
+export const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginScreen />}/>
+            <Route path="/*" element={<CalendarScreen />}/>
+        </Routes>
+    )
+}
